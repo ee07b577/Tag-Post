d262c7d8ccd11126dceb96a2dff7d3bc12b4b043
Sunny <s@zhso.net>
2016-11-30 23:09:39 +0800

    选项页点击保存后，自动关闭

--- app/js/options.js
+++ app/js/options.js
@@ -1,23 +1,21 @@
 $(function() {
     chrome.storage.sync.get('options', function(data) {
-        var options = data.options;
-        if (options) {
-            for (var j in options) {
-                $('input[data-option="' + j + '"]').val(options[j]);
+        if (data && data.options) {
+            var options = data.options;
+            for (var i in options) {
+                $(`input[data-option="${i}"]`).val(options[i]);
             }
         }
     });
     $('#btnSave').click(function() {
         var options = {};
         $('input[data-option]').each(function(index, input) {
-            var key = $(input).data('option');
-            var val = $(input).val();
-            options[key] = val;
+            options[$(input).data('option')] = $(input).val();
         });
         chrome.storage.sync.set({
             'options': options
         }, function() {
-            log('option saved.');
+            window.close();
         })
     });
 });

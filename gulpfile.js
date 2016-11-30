4059556b7fb7b292fd92fa674d4dfc3a4ce46d4e
Sunny <s@zhso.net>
2016-12-01 00:36:20 +0800

    清理编译后无用文件

--- gulpfile.js
+++ gulpfile.js
@@ -1,11 +1,11 @@
 'use strict';
 const gulp = require('gulp');
 gulp.task('build', () => {
-    gulp.src(['node_modules/bootstrap/dist/*/*.*']).pipe(gulp.dest('dist'));
-    gulp.src(['node_modules/jquery/dist/*.*']).pipe(gulp.dest('dist/js'));
-    gulp.src(['app/*/*.*', 'app/*.*']).pipe(gulp.dest('dist'));
+    gulp.src(['node_modules/bootstrap/dist/*/*.min.*', 'app/*/*.*', 'app/*.*']).pipe(gulp.dest('dist'));
+    gulp.src('node_modules/jquery/dist/jquery.min.*').pipe(gulp.dest('dist/js'));
+    gulp.src('node_modules/bootstrap/dist/fonts/*.*').pipe(gulp.dest('dist/fonts'));
     gulp.src('app/_locales/*/*.*').pipe(gulp.dest('dist/_locales'));
 });
 gulp.task('default', () => {
-    gulp.watch(['app/*/*.*', 'app/*.*'], ['build']);
+    gulp.watch(['app/*/*.*', 'app/*.*', 'app/_locales/*/*.*'], ['build']);
 });

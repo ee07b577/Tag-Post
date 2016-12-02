52d158f78d517a0ea209f1573949ded5ca2e4812
邵振华 <s@zhso.net>
2016-12-02 14:43:40 +0800

    解决了图片无法显示标记框的问题

--- app/js/i18n.js
+++ app/js/i18n.js
@@ -1,5 +1,5 @@
-$(function() {
-    $('*[data-i18n]').each(function() {
+$(function () {
+    $('*[data-i18n]').each(function () {
         $(this).text(chrome.i18n.getMessage($(this).data('i18n')));
     });
 });
